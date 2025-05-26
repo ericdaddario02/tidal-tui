aca92ec3f3240d69a961e5d063f68be1199e7579
Eric D'Addario <ericdaddario02@gmail.com>
2025-05-26 18:30:42 -0400

    Add example code for streaming audio from url with stream-download (requires async).

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::process;
+use stream_download::{storage::{memory::MemoryStorageProvider}, Settings, StreamDownload};
+use tokio;
 use rodio::{Decoder, OutputStream, Sink};
 
 use tidal_tui::rtidalapi;
 use rtidalapi::{AudioQuality, Session};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let session = Session::new_oauth().unwrap_or_else(|err| {
         println!("{err}");
         process::exit(1);
@@ -28,10 +31,50 @@ fn main() {
     let sink = Sink::try_new(&stream_handle).unwrap();
     sink.set_volume(0.1);
 
+    play_stream_example(&sink, &track_url).await;
+    // play_file_example(&sink);
+}
+
+async fn play_stream_example(sink: &Sink, url: &String) {
+    let reader = StreamDownload::new_http(
+        url.parse().unwrap(),
+        MemoryStorageProvider,
+        Settings::default(),
+    ).await.unwrap();
+
+    let source = Decoder::new(reader).unwrap();
+    sink.append(source);
+    // sink.try_seek(std::time::Duration::from_secs(20)).unwrap();
+    // sink.try_seek(std::time::Duration::from_secs(0)).unwrap();
+    sink.sleep_until_end();
+}
+
+// fn play_stream_example2(sink: &Sink, url: &String) {
+//     let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+
+//     let future = async {
+//         let reader = StreamDownload::new_http(
+//             url.parse().unwrap(),
+//             MemoryStorageProvider,
+//             Settings::default(),
+//         ).await.unwrap();
+
+//         let source = Decoder::new(reader).unwrap();
+//         sink.append(source);
+//         // sink.try_seek(std::time::Duration::from_secs(20)).unwrap();
+//         // sink.try_seek(std::time::Duration::from_secs(0)).unwrap();
+//         sink.sleep_until_end();
+//     };
+
+//     rt.block_on(future);
+// }
+
+fn play_file_example(sink: &Sink) {
     let file = std::io::BufReader::new(std::fs::File::open("song.flac").unwrap());
+
     let source = Decoder::new(file).unwrap();
     sink.append(source);
-    sink.try_seek(std::time::Duration::from_secs(20)).unwrap();
-    sink.try_seek(std::time::Duration::from_secs(0)).unwrap();
+    // sink.try_seek(std::time::Duration::from_secs(20)).unwrap();
+    // sink.try_seek(std::time::Duration::from_secs(0)).unwrap();
     sink.sleep_until_end();
 }
